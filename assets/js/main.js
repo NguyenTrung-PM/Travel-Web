289ba4a1272047c8b29ee6dd094598f92e4cb85f
NguyenTrung-PM <ntcoder.contact@gmail.com>
2022-01-08 13:31:26 +0700

    Responsive and scroll reveal animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,25 +98,50 @@ videoFile.addEventListener('ended', () => {
 });
 
 // TODO: DARK LIGHT THEME
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'ri-sun-line'
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'ri-sun-line';
 
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
 
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? 'dark' : 'light');
+const getCurrentIcon = () =>
+     themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line';
 
 if (selectedTheme) {
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+     document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+     themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme);
 }
 
 themeButton.addEventListener('click', () => {
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
+     document.body.classList.toggle(darkTheme);
+     themeButton.classList.toggle(iconTheme);
+
+     localStorage.setItem('selected-theme', getCurrentTheme());
+     localStorage.setItem('selected-icon', getCurrentIcon());
+});
+
+// TODO: SCROLL REVEAL ANIMATION
+const sr = ScrollReveal({
+     distance: '60px',
+     duration: 2800,
+     // reset:true,
+});
+
+sr.reveal(
+     `.home__data, .home__social-link,.home__info, .discover__container, .experience__data, .experience__overlay, .place__card, .sponsor__content, .footer__data, .footer__rights`,
+     {
+          origin: 'top',
+          interval: 100,
+     }
+);
+
+sr.reveal(`.about__data, .video__description, .subscribe__description`, {
+     origin: 'left',
+});
+
+sr.reveal(`.about__img-overlay,.video__content,.subscribe__form`, {
+     origin: 'right',
+     interval: 100,
+});
