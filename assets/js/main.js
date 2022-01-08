268fd11cba44c9c68999a867416ba6c0383f95b2
NguyenTrung-PM <ntcoder.contact@gmail.com>
2022-01-08 13:11:37 +0700

    Dark theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,7 +26,7 @@ navLink.forEach((link) =>
      })
 );
 
-// TODO: CHANGE BACKGROUND COLOR
+// TODO: CHANGE BACKGROUND COLOR AND SHOW SCROLL UP
 window.addEventListener('scroll', () => {
      const header = document.getElementById('header');
      if (this.scrollY >= 200) {
@@ -34,6 +34,32 @@ window.addEventListener('scroll', () => {
      } else {
           header.classList.remove('scroll-header');
      }
+
+     const scrollUp = document.getElementById('scroll-up');
+     if (this.scrollY >= 560) {
+          scrollUp.classList.add('show-scroll');
+     } else {
+          scrollUp.classList.remove('show-scroll');
+     }
+
+     const sections = document.querySelectorAll('section[id]');
+     const scrollY = window.pageYOffset;
+
+     sections.forEach((section) => {
+          const sectionHeight = section.offsetHeight;
+          const sectionTop = section.offsetTop - 50;
+          let sectionId = section.getAttribute('id');
+
+          if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+               document
+                    .querySelector('.nav__menu a[href*=' + sectionId + ']')
+                    .classList.add('active-link');
+          } else {
+               document
+                    .querySelector('.nav__menu a[href*=' + sectionId + ']')
+                    .classList.remove('active-link');
+          }
+     });
 });
 
 // TODO: SWIPER JS
@@ -70,3 +96,27 @@ videoFile.addEventListener('ended', () => {
      videoIcon.classList.remove('ri-pause-line');
      videoIcon.classList.add('ri-play-line');
 });
+
+// TODO: DARK LIGHT THEME
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
